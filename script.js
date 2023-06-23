9b79e582add9df2afa562ec1f1bc673ecb6282f0
Nugraha Panca Wibisana <111733086+NugrahaPancaWibisana@users.noreply.github.com>
2023-06-23 21:45:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function red() {
+    document.getElementById("background").style.backgroundColor = "red";
+}
+
+function green() {
+    document.getElementById("background").style.backgroundColor = "green";
+}
+
+function blue() {
+    document.getElementById("background").style.backgroundColor = "blue";
+}
+
+function reset() {
+    document.getElementById("background").style.backgroundColor = "white";
+}
